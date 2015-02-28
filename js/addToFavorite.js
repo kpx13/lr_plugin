61eb1bcc68d275f9171b69b1c6f03ef342c80685
Konstantin Kutsyllo <trikadin@mail.ru>
2015-03-01 00:22:33 +0300

    Initial version of addToFavorite.js

--- js/addToFavorite.js
+++ js/addToFavorite.js
@@ -1,5 +1,11 @@
 /**
- * @fileOverview , date 28.02.2015:
+ * @fileOverview date 28.02.2015:
  * @author <trikadin@mail.ru>
  * @version 0.1
  */
+
+function iconClickListener(tab) {
+    alert("1, сука.");
+}
+
+chrome.browserAction.onClicked.addListener(iconClickListener);
